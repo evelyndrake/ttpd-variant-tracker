4c3a15abe647167ef39d42e19875c73fecb4e732
Evelyn Drake <128602777+evelyndrake@users.noreply.github.com>
2024-06-14 15:31:00 -0400

    Added table of variants

--- app.js
+++ app.js
@@ -36,6 +36,56 @@ app.get('/variants', async function(request, response) {
 	}
 });
 
+// Get array of variants with their information iterating through all pages
+app.get('/allvariants', async function(request, response) {
+	try {
+		const url = 'https://api.discogs.com/masters/3461018/versions'; 
+		const discogsResponse = await axios.get(url, {
+			headers: { // Send the user agent and authorization headers
+				'User-Agent': userAgent,
+				'Authorization': `Discogs token=${discogsToken}`
+			}
+		});
+		const items = discogsResponse.data.pagination.items;
+		const pages = discogsResponse.data.pagination.pages;
+		const variants = [];
+		for (let i = 1; i <= pages; i++) {
+			const pageUrl = `${url}?page=${i}`;
+			const pageResponse = await axios.get(pageUrl, {
+				headers: { // Send the user agent and authorization headers
+					'User-Agent': userAgent,
+					'Authorization': `Discogs token=${discogsToken}`
+				}
+			});
+			variants.push(...pageResponse.data.versions);
+		}
+		// console.log(variants);
+		response.send(variants); // Send the array of variants
+	} catch (error) { // Handle errors
+		console.error(error);
+		response.status(500).send('Internal Server Error');
+	}
+});
+
+// Get formats for a specific ID
+app.get('/formats/:id', async function(request, response) {
+	try {
+		const id = request.params.id;
+		const url = `https://api.discogs.com/releases/${id}`;
+		const discogsResponse = await axios.get(url, {
+			headers: { // Send the user agent and authorization headers
+				'User-Agent': userAgent,
+				'Authorization': `Discogs token=${discogsToken}`
+			}
+		});
+		const formats = discogsResponse.data.formats;
+		response.send(formats); // Send the formats
+	} catch (error) { // Handle errors
+		console.error(error);
+		response.status(500).send('Internal Server Error');
+	}
+});
+
 // Start the server
 app.listen(port,function(){
 	console.log('Listening at port',port);
--- assets/api.js
+++ assets/api.js
@@ -1,9 +1,52 @@
+// Update the initial number
 fetch('/variants') // Fetch the number of variants from the server
     .then(response => response.text())
     .then(data => { // Update the text content of the element with the number of variants
         const element = document.getElementById('varNum');
         element.textContent = data;
     })
-    .catch(error => { // Handle errors
+    .catch(error => { 
         console.error('Error:', error);
     });
+
+// Hide table before loading
+document.getElementById('varTable').style.display = 'none';
+
+// Populate the table
+fetch('/allvariants') // Fetch the array of variants from the server
+    .then(response => response.json())
+    .then(data => { // Populate the table with the array of variants
+        const table = document.getElementById('varTableBody');
+        var i = 0;
+        data.forEach(variant => {
+            const varFormats = variant.major_formats.join(', '); // Combine the formats into a single string
+            const url = "https://www.discogs.com/release/"+variant.id; // Create the URL for the release
+            const row = table.insertRow();
+            row.insertCell().textContent = ++i; // Add a row number
+            // Add an image to the cell
+            const imageUrl = variant.thumb;
+            const imgCell = row.insertCell();
+            const img = document.createElement('img');
+            img.style.width = '75px';
+            img.style.height = '75px';
+            img.src = imageUrl ? imageUrl : "https://i.imgur.com/E5yByRE.png";
+            imgCell.appendChild(img);
+            // Add a link to the cell
+            const titleCell = row.insertCell();
+            const link = document.createElement('a');
+            link.href = url;
+            link.target = '_blank';
+            link.textContent = variant.title + " ["+variant.format+"]";
+            titleCell.appendChild(link);
+            // Add the rest of the cells
+            row.insertCell().textContent = varFormats
+            row.insertCell().textContent = variant.released;
+            row.insertCell().textContent = variant.label;
+            row.insertCell().textContent = variant.country;
+        });
+        document.getElementById('varTable').style.display = 'table';
+    })
+    .catch(error => { 
+        console.error('Error:', error);
+    });
+
